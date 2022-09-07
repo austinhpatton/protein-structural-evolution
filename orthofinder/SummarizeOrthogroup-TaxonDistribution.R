d9a772d2f11e3e9b0a0ef9d56f794c63c8e862d9
austinhpatton <austin.patton@wsu.edu>
2022-09-06 18:01:14 -0700

    Several changes...

--- orthofinder/SummarizeOrthogroup-TaxonDistribution.R
+++ orthofinder/SummarizeOrthogroup-TaxonDistribution.R
@@ -27,6 +27,10 @@ source('ArcadiaColorBrewer.R')
 # A General plotting function to generate many discrete colors
 getPalette = colorRampPalette(RColorBrewer::brewer.pal(9, "Set1"))
 
+# Read in metadata - we want to know the number of species per supergroup
+metadat <- read_tsv('../../../Protein-SeqStruct-Evolution/EukProt-V3-Data/TCS_EukProt_included_data_sets.v03.2021_11_22.txt')
+supgrp.spp.counts <- summary(as.factor(metadat$Supergroup_UniEuk))
+
 # Read in the summaries
 spp.per.og <- read_tsv(paste0(gitdir, 'og_taxonomy_genecount_summaries/NumSpecies_PerSupergroup_InOGs.tsv'))
 count.per.og <- read_tsv(paste0(gitdir, 'og_taxonomy_genecount_summaries/NumGeneCopies_PerSupergroup-InOGs.tsv'))
@@ -133,6 +137,32 @@ ogs.per.grp.plt <-
         axis.text.x = element_text(angle = 45, hjust = 1),
           axis.title.x = element_blank())
 
+# Now what if we account for the number of species in each orthogroup?
+ogs.per.grp$SppInSupergroup <- 
+  supgrp.spp.counts[match(ogs.per.grp$Supergroup, 
+                          names(supgrp.spp.counts))]
+
+num.spp.in.grp.v.num.ogs.plt <- 
+  ggplot(data = ogs.per.grp, aes(x = SppInSupergroup, y = NumOGs, fill = Supergroup)) + 
+  geom_point(pch = 21, size = 5, alpha = 0.8) + 
+  scale_y_log10() + 
+  annotation_logticks(sides = 'l') + 
+  theme_bw(base_size = 14) + 
+  xlab("Number of Species in Supergroup") +
+  ylab("Number of OGs") + 
+  scale_fill_manual(values = getPalette(31)) +
+  labs(fill = "Supergroup") +
+  theme(legend.position = 'top')
+
+# Why don't we fit a linear model that accounts for multiple things
+# predict number of orthogroups for each supergroup, given...
+# The number of species in that supergroup 
+# The total number of proteins in species within that supergroup
+# The age of the supergroup
+ogs.per.grp.lm <- lm(NumOGs ~ SppInSupergroup, data = ogs.per.grp)
+jtools::plot_summs(ogs.per.grp.lm, plot.distributions = TRUE)
+
+
 og.count.summ.plts <- 
   plot_grid(spp.per.og.plt, grps.per.og.plt, 
             spp.per.og.hist, grp.per.og.hist,
@@ -178,9 +208,8 @@ og.spp.inclusion.plt <-
             size = 3, color = 'black') + 
   geom_vline(xintercept = 5, color = 'blue', lty = 2, size = 1, alpha = 0.5) +
   geom_vline(xintercept = 10, color = 'red', lty = 2, size = 1, alpha = 0.5) +
-  scale_x_reverse() + 
-  xlab('Cumul. Prop. of Orthologs') +
-  ylab('Number of Species in Orthogroup') +
+  ylab('Cumul. Prop. of Orthologs') +
+  xlab('Number of Species in Orthogroup') +
   scale_color_manual(values = getPalette(31)) +
   theme_bw(base_size = 14) +
   theme(legend.position = 'top') +
@@ -299,4 +328,53 @@ ggsave(per.tax.count.umap$umap.plot,
        file = "PerSupergroup_GeneCount_UMAP.png",
        height = 8, width = 8)
 
+
+############################################################################
+# What do things look like when we consider orthogroups that have on average <= 3 gene copies, 
+# per species, and include at least 10 species?
+core.ogs <- 
+  spp.per.og$Orthogroup[which(c(spp.per.og$TotalNumGenes / spp.per.og$NumSppInOG) >= 1 & 
+                                c(spp.per.og$TotalNumGenes / spp.per.og$NumSppInOG) <= 3 & 
+                                spp.per.og$NumSppInOG >= 10)] 
+
+# Read in the summaries
+spp.per.core.og <- spp.per.og[which(spp.per.og$Orthogroup %in% core.ogs),]
+count.per.core.og <- count.per.og[which(count.per.og$Orthogroup %in% core.ogs),]
+prop.count.per.core.og <- prop.count.per.og[which(prop.count.per.og$Orthogroup %in% core.ogs),]
+
+# The number of gene copies per orthogroup size (number of species included)
+spp.per.core.og.plt <- 
+  ggplot(data = spp.per.core.og, aes(x = NumSppInOG, y = TotalNumGenes, color = NumGrps)) + 
+  geom_point(size = 3, stroke = 0.1, alpha = 0.75) + 
+  theme_bw(base_size = 14) + 
+  xlab("# of Species in OG") +
+  ylab("Total # of Gene Copies") + 
+  scale_color_viridis_c(option = 'B') + 
+  labs(color = "# of Supergroups") +
+  theme(legend.position = 'top')
+
+
+# Summarize how many orthogroups include each number of species
+spp.core.og.hist.dat <- data.frame(NumSppInOG = unique(factor(spp.per.core.og$NumSppInOG)),
+                              NumOGs = NA, NumGrps = NA)
+spp.core.og.hist.dat <- spp.core.og.hist.dat[order(spp.core.og.hist.dat$NumSppInOG),]
+for(i in 1:nrow(spp.core.og.hist.dat)){
+  spp.core.og.hist.dat$NumOGs[i] <- length(which(spp.per.core.og$NumSppInOG == i+9))
+  spp.core.og.hist.dat$NumGrps[i] <- mean(spp.per.core.og$NumGrps[which(spp.per.core.og$NumSppInOG == i+9)])
+}
+spp.core.og.hist.dat$NumSppInOG <- as.numeric(as.character(spp.core.og.hist.dat$NumSppInOG))
+spp.per.core.og.hist <- 
+  ggplot(data = spp.core.og.hist.dat, 
+         aes(x = NumSppInOG, 
+             y = NumOGs, 
+             fill = NumGrps)) + 
+  geom_point(pch = 21, alpha = 0.8, size = 3) +
+  scale_y_log10() +
+  annotation_logticks(sides = 'l') + 
+  scale_fill_viridis_c(option = 'B') +
+  xlab("# of Species in OG") +
+  ylab("# of Orthogroups") +
+  theme_bw(base_size = 14) +
+  labs(fill = "# of Supergroups in OG") +
+  theme(legend.position = 'top')
 
